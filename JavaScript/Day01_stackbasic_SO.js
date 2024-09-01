d9899069d02c96b38c0071b9a33c5a271c0900e0
Saumya Subham Mishra <109909356+SaumyaSubham@users.noreply.github.com>
2024-09-01 22:29:44 +0530

    Add files via upload

---
+++ JavaScript/Day01_stackbasic_SO.js
@@ -0,0 +1,16 @@
+// Stack class
+class Stack {
+
+    // Array is used to implement stack
+    constructor()
+    {
+        this.items = [];
+    }
+
+    // Functions to be implemented
+    // push(item)
+    // pop()
+    // peek()
+    // isEmpty()
+    // printStack()
+}
